84b32a70cfe7d3530806e3870671a904d81addd3
saisilinus <saisilinus19@gmail.com>
2022-02-23 11:51:46 +0300

    feat: replaced autogenerated _Id

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,37 +1,26 @@
-import { MongoClient, Db } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import config from './config';
-import logger from './logger';
 
 const uri = config.mongoose.url;
 const options = {};
 
 let client: MongoClient | undefined = undefined;
-let db: Db | undefined = undefined;
 let clientPromise: Promise<MongoClient> | undefined = undefined;
 
-(async function(){
-    if (config.env === 'development') {
-        // In development mode, use a global variable so that the value
-        // is preserved across module reloads caused by HMR (Hot Module Replacement).
-        if (!global._mongoClientPromise) {
-            client = new MongoClient(uri, options)
-            global._mongoClientPromise = client.connect()
-    }
-    
-    clientPromise = global._mongoClientPromise
-    
-    } else {
-        // In production mode, it's best to not use a global variable.
-        client = new MongoClient(uri, options);
-        clientPromise = client.connect()
-    }
+if (config.env === 'development') {
+    // In development mode, use a global variable so that the value
+    // is preserved across module reloads caused by HMR (Hot Module Replacement).
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(uri, options)
+        global._mongoClientPromise = client.connect()
+}
 
-    await client?.connect();
-    if (client){
-        logger.info('Connected to MongoDB');
-        db = client.db('analytics-web-app');
-    }
-    
-})();
+clientPromise = global._mongoClientPromise
+
+} else {
+    // In production mode, it's best to not use a global variable.
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect()
+}
 
 export { clientPromise, client };
--- services/user.services.ts
+++ services/user.services.ts
@@ -1,14 +1,16 @@
 import { clientPromise, client } from '../lib/mongodb';
 import { InsertOneResult } from 'mongodb';
+import config from '../lib/config';
 
 /**
  * Insert a user
  * @param {} userBody
  */
 export const insertUser = async (userBody: Record<string, any>) => {
+    Object.assign(userBody, { _id: userBody.userId })
     await clientPromise;
     if (client){
-        const db = client.db('analytics-web-app');
+        const db = client.db(config.mongoose.dbName);
         const users = db.collection('users');
         const result: InsertOneResult | undefined = await users?.insertOne(userBody);
         return result;
