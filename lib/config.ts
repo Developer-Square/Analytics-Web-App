c3367140660e81e5d867c625f74a3bf011614cef
Ryan Njoroge <keddelyronjoz@gmail.com>
2022-02-23 14:31:23 +0300

    feat: Added custom config validator

---
+++ lib/config.ts
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+
+const envVarsSchema = Joi.object().keys({
+    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+    MONGODB_URI: Joi.string().required().description('Mongo DB uri'),
+});
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+const config = {
+    env: envVars.NODE_ENV,
+    mongoose: {
+        url: envVars.MONGODB_URI + (envVars.NODE_ENV === 'test' ? 'test' : ''),
+    },
+};
+
+export default config;
