b56e53e9890418c72dce8ba4ee90fcaa35298dff
saisilinus <saisilinus19@gmail.com>
2022-02-23 15:33:02 +0300

    prevent duplication of userId

--- services/user.services.ts
+++ services/user.services.ts
@@ -27,7 +27,7 @@ export class User {
      * @returns {Promise<InsertOneResult<Document>>}
      */
     async insertUser(userBody: Record<string, any>): Promise<InsertOneResult<Document>>{
-        Object.assign(userBody, { _id: userBody.userId })
+        delete Object.assign(userBody, {['_id']: userBody['userId'] })['userId'];
         return this.db.collection('users').insertOne(userBody);
     }
 
