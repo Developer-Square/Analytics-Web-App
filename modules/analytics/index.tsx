050a11cb1a02276f52479cf62c7eb749e18a91ba
Ryan Njoroge <keddelyronjoz@gmail.com>
2022-02-23 10:44:22 +0300

    feat: Added aws and stitch pipeline

--- modules/analytics/index.tsx
+++ modules/analytics/index.tsx
@@ -17,31 +17,71 @@ interface UserIdentifyEventProps {
     username: string
 }
 
+const sendData = (data: any) => {
+    console.log(typeof (data));
+    if (typeof navigator.sendBeacon === 'function') {
+        navigator.sendBeacon('https://t4odney23k.execute-api.us-east-1.amazonaws.com/default/postEventData', JSON.stringify(data))
+    } else {
+        var xhr = new XMLHttpRequest()
+        xhr.open('POST', 'https://t4odney23k.execute-api.us-east-1.amazonaws.com/default/postEventData')
+        xhr.send(data)
+    }
+}
+
+const awsPlugin = () => {
+    return {
+        name: 'aws-plugin',
+        page: function page(_ref: any) {
+            console.log(typeof (_ref));
+            const payload = _ref.payload
+            console.log('Page Event', payload);
+            sendData(payload)
+        },
+        track: function track(_ref2: any) {
+            console.log(typeof (_ref2));
+            const payload = _ref2.payload
+            sendData(payload)
+        },
+        identify: function identify(_ref3: any) {
+            console.log(typeof (_ref3));
+            const payload = _ref3.payload
+            sendData(payload)
+        },
+        loaded: function loaded() {
+            return true
+        }
+    }
+}
+
 const analytics = Analytics({
     app: 'small-scale-ecommerce',
     plugins: [
         googleAnalytics({
             trackingId: 'UA-221105984-1'
-        })
+        }),
+        awsPlugin()
     ],
     debug: true
 })
 
+// Checkout this link for a better explanation of the following code:
+// https://getanalytics.io/tutorials/typesafe-analytics/
+// https://youtu.be/NJxagi7K-D8
 export interface EventMap {
     checkout: CheckoutEventProps,
     addToCart: AddToCartEventProps,
     identifyUser: UserIdentifyEventProps
 }
 
-export function trackEvent<K extends keyof EventMap>(eventName: K, props: EventMap[K]): void {
+export const trackEvent = <K extends keyof EventMap>(eventName: K, props: EventMap[K]): void => {
     analytics.track(eventName, props)
 }
 
-export function pageVisit(): void {
+export const pageVisit = (): void => {
     analytics.page()
 }
 
-export function userIdentify(params: UserIdentifyEventProps): void {
+export const userIdentify = (params: UserIdentifyEventProps): void => {
     analytics.identify(params.userId, { email: params.email, name: params.username })
 }
 
