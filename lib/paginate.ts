c87aaa17e6295a1ff5ed87a08c2ae2b38879f61a
Ryan Njoroge <keddelyronjoz@gmail.com>
2022-02-23 15:42:51 +0300

    feat: Added filter and pagination functionality

---
+++ lib/paginate.ts
@@ -0,0 +1,58 @@
+import { Collection } from "mongodb";
+import logger from "./logger";
+
+export interface IOptions {
+    sortBy: string;
+    populate: string;
+    limit: number;
+    page: number;
+}
+
+export default class Paginate {
+    filter: Record<string, any>;
+    sort: string;
+    limit: number;
+    page: number;
+    collection: Collection;
+    count: Promise<number>;
+    documents;
+
+    constructor(filter: Record<string, any>, options: IOptions, collection: Collection) {
+        this.filter = filter;
+        this.sort = this.sanitizeSort(options.sortBy);
+        this.limit = this.sanitizelimit(options.limit);
+        this.page = this.sanitizePage(options.page);
+        this.collection = collection;
+        this.count = this.countDocs();
+        this.documents = this.findDocs();
+    }
+
+    sanitizeSort(sortBy: string): string {
+        if (sortBy) {
+            const sortingCriteria: any = [];
+            sortBy.split(',').forEach((sortOption: string) => {
+                const [key, order] = sortOption.split(':');
+                sortingCriteria.push((order === 'desc' ? '-' : '') + key);
+            });
+            return sortingCriteria.join(' ');
+        } else {
+            return 'createdAt';
+        }
+    }
+
+    sanitizelimit(limit: number): number {
+        return limit && parseInt(limit.toString(), 10) > 0 ? parseInt(limit.toString(), 10) : 10;
+    }
+
+    sanitizePage(page: number): number {
+        return page && parseInt(page.toString(), 10) > 0 ? parseInt(page.toString(), 10) : 1;
+    }
+
+    async countDocs(): Promise<number> {
+        return await this.collection.countDocuments(this.filter);
+    }
+
+    async findDocs() {
+        return await this.collection.find(this.filter).sort(this.sort).skip((this.page - 1) * this.limit).limit(this.limit).toArray();
+    }
+};
--- pages/api/users.ts
+++ pages/api/users.ts
@@ -11,5 +11,8 @@ export default async function handler(
     if (req.method === 'POST') {
         const result = await UserCollection.insertUser(req.body);
         res.status(200).json({ result })
+    } else if (req.method === 'GET') {
+        const docs = await UserCollection.paginate(req.body);
+        res.status(200).json({ results: docs })
     }
 }
