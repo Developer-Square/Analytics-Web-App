50bb8958d1b6553cc2e1a8ef2a66e42b5a00989d
saisilinus <saisilinus19@gmail.com>
2022-02-23 14:42:04 +0300

    feat: add filter+ pagination

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -8,9 +8,13 @@ export default async function handler(
 ){
     const UserCollection = new User(client);
     await User.build();
-    
+
     if (req.method === 'POST') {
         const result = await UserCollection.insertUser(req.body);
         res.status(200).json({ result })
+    }
+    else if (req.method === 'GET') {
+        const docs = await UserCollection.paginate(req.body);
+        res.status(200).json({ results: docs })
     }
 }
--- services/user.services.ts
+++ services/user.services.ts
@@ -1,23 +1,58 @@
 import { clientPromise, client } from '../lib/mongodb';
-import { MongoClient } from 'mongodb';
+import { Db, DeleteResult, MongoClient, InsertOneResult } from 'mongodb';
 import config from '../lib/config';
+import Paginate from '../lib/paginate';
 
 export class User {
     client: MongoClient;
+    db: Db;
 
     constructor(client: MongoClient | undefined){
         if (!client) throw new Error('Cannot be called directly');
         this.client = client;
+        this.db = client.db(config.mongoose.dbName);
     }
 
+    /**
+     * Ensures the client is defined i.e. not undefined
+     * @returns
+     */
     static async build () {
         await clientPromise;
         return new User(client)
     }
 
-    async insertUser(userBody: Record<string, any>){
+    /**
+     * Insert as single user
+     * @returns {Promise<InsertOneResult<Document>>}
+     */
+    async insertUser(userBody: Record<string, any>): Promise<InsertOneResult<Document>>{
         Object.assign(userBody, { _id: userBody.userId })
-        const db = this.client.db(config.mongoose.dbName);
-        return db.collection('users').insertOne(userBody);
+        return this.db.collection('users').insertOne(userBody);
+    }
+
+    /**
+     * Delete a single user
+     * @returns {Promise<DeleteResult>}
+     */
+    async deleteUser(userId: string): Promise<DeleteResult>{
+        return this.db.collection('users').deleteOne({ "_id":userId });
+    }
+
+    /**
+     * This will reset the users collection by deleting all documents. Use carefully
+     * @returns {Promise<DeleteResult>}
+     */
+    async reset(): Promise<DeleteResult>{
+        return this.db.collection('users').deleteMany({});
+    }
+
+    /**
+     * Paginates users
+     * @param paginationbody pagination filter and options
+     */
+    async paginate(paginationbody: Record<string, any>) {
+        const pagination = new Paginate(paginationbody.filter, paginationbody.options, this.db.collection('users'));
+        return await pagination.findDocs();
     }
 }
