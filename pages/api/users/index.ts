f2f83976a01e4eb9a41aede1e5ffdf31cc5225f6
saisilinus <saisilinus19@gmail.com>
2022-02-24 14:57:49 +0300

    feat: handled common errors

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -2,11 +2,12 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import httpStatus from 'http-status';
 import { User } from '../../../services/user.services';
 import { client } from '../../../lib/mongodb';
+import catchAPIError from '../../../lib/catchAPIError';
 
-export default async function handler(
+export default catchAPIError(async (
     req: NextApiRequest,
     res: NextApiResponse
-){
+) => {
     const UserCollection = new User(client);
     await User.build();
 
@@ -21,4 +22,24 @@ export default async function handler(
     else {
         res.status(httpStatus.NOT_FOUND);
     }
-}
+})
+
+// export async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse
+// ){
+//     const UserCollection = new User(client);
+//     await User.build();
+
+//     if (req.method === 'POST') {
+//         const result = await UserCollection.insertUser(req.body);
+//         res.status(httpStatus.CREATED).json({ result })
+//     }
+//     else if (req.method === 'GET') {
+//         const docs = await UserCollection.paginate(req.body);
+//         res.status(httpStatus.OK).json({ results: docs })
+//     }
+//     else {
+//         res.status(httpStatus.NOT_FOUND);
+//     }
+// }
