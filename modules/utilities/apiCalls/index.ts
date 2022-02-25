d773020c5bf61d5b8d4b65a69c7c39c30a5576dd
Ryan Njoroge <keddelyronjoz@gmail.com>
2022-02-25 16:50:04 +0300

    feat: Added a custom error handler

--- modules/utilities/apiCalls/index.ts
+++ modules/utilities/apiCalls/index.ts
@@ -1,32 +1,41 @@
+import axios from "axios"
 interface postDataParams {
-    method: string,
     path: string,
-    data: any
+    payload: any
 }
 interface getDataParams {
-    method: string,
     path: string
 }
 
-const sendUserDataToApi = async ({ method, path, data }: postDataParams) => {
-    const response = await fetch(path,
-        {
-            method,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
+export default axios.create({
+    headers: {
+        "Content-type": "application/json"
+    }
+})
+
+const sendUserDataToApi = async ({ path, payload }: postDataParams) => {
+    const response = await fetch(path, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
     return response.json()
 }
 
-const getUserDataFromApi = async ({ method, path }: getDataParams) => {
+const getUserDataFromApi = async ({ path }: getDataParams) => {
     const response = await fetch(path, {
-        method,
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
     })
+    if (!response.ok) {
+        if (response.status === 404) {
+            throw new Error('Page was not found. Please try again.')
+        }
+    }
     return response.json()
 }
 
