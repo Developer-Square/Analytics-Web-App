715920367486155607a139a91be1307cdbccd856
ryann254 <keddelyronjoz@gmail.com>
2023-05-07 09:45:49 +0300

    fix: add load times to Display page to prevent infinite loop

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -3,18 +3,16 @@ import catchAPIError from '@/modules/errors/catchAPIError';
 import seed from '@/modules/database/populate';
 import config from '@/modules/config/config';
 
-export default catchAPIError(async (
-    req: NextApiRequest,
-    res: NextApiResponse
-) => {
-    if(config.env !== 'development'){
-        res.status(404).end();
+export default catchAPIError(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    console.log(config.env);
+    if (config.env !== 'development') {
+      res.status(404).end();
+    } else if (req.method === 'POST') {
+      const result = await seed();
+      res.status(200).json({ result });
+    } else {
+      res.status(404).end();
     }
-    else if(req.method === 'POST'){
-        const result = await seed();
-        res.status(200).json({ result });
-    }
-    else {
-        res.status(404).end();
-    }
-})
+  }
+);
